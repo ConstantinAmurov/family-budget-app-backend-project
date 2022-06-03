84dda17b23e646593dce16fc0425be8522d692aa
CostelMD <amurov.constantin@gmail.com>
2022-06-03 12:58:45 +0200

    Add Budget Routing + verify CSRF token

--- app/Http/Controllers/BudgetController.php
+++ app/Http/Controllers/BudgetController.php
@@ -2,18 +2,90 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Budget;
 use Illuminate\Http\Request;
 
 class BudgetController extends Controller
 {
     /**
-     * Handle the incoming request.
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $budgets = Budget::all();
+
+        return $budgets;
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function __invoke(Request $request)
+    public function store(Request $request)
     {
         //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Budget  $budget
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Budget $budget)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Budget  $budget
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Budget $budget)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Budget  $budget
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Budget $budget)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Budget  $budget
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $budget = Budget::findOrFail($id);
+        if($budget) {
+            $budget->delete();
+        }
+        return $budget;
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\BudgetController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/budgets', [BudgetController::class,'index']);
+
+Route::delete('/budgets/{id}', [BudgetController::class,'destroy']);
+
